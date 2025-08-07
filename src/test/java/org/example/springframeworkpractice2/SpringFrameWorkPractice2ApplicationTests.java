8d9f068cf9fe085e95b2af73eff0dd0709354b52
somminn <cowmin104@gmail.com>
2025-08-07 12:10:25 +0900

    [Drink] commit

---
+++ src/test/java/org/example/springframeworkpractice2/SpringFrameWorkPractice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springframeworkpractice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameWorkPractice2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
