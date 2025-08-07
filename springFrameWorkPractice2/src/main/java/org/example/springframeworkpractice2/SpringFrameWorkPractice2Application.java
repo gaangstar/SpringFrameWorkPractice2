3dc63555bb630f086f22a347b643f0c3c9d3e1f5
yhg <hghgy@hansung.ac.kr>
2025-08-07 10:44:50 +0900

    first commit

---
+++ springFrameWorkPractice2/src/main/java/org/example/springframeworkpractice2/SpringFrameWorkPractice2Application.java
@@ -0,0 +1,13 @@
+package org.example.springframeworkpractice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameWorkPractice2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFrameWorkPractice2Application.class, args);
+    }
+
+}
