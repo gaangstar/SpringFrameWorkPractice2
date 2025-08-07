e1add81fffe3f3d0dd887f60588a4972cb688147
somminn <cowmin104@gmail.com>
2025-08-07 11:47:07 +0900

    [Review] commit

--- src/main/java/org/example/springframeworkpractice2/review/controller/ReviewController.java
+++ src/main/java/org/example/springframeworkpractice2/review/controller/ReviewController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/")
+@RequestMapping("/review")
 public class ReviewController {
     private final ReviewService reviewService;
 
